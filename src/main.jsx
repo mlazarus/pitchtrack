8a9378007be8aaa7231f71cb519816100aee6ecd
mlazarus <mikelaz@yahoo.com>
2026-02-12 21:10:53 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PitchGameTracker from './PitchGameTracker'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PitchGameTracker />
+  </React.StrictMode>,
+)
